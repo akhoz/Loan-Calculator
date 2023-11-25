26cf93c58697478d5a458458af27c3f89522c125
akhoz <adrian25092005@gmail.com>
2023-11-25 00:15:13 -0600

    final release

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+      <div className="flex items-center justify-center mt-10">
+          <a href="https://github.com/akhoz/Loan-Calculator">
+              <img
+                  src={"public/github-logo.png"}
+                  alt="Github Logo"
+                  className="w-32 h-32 object-cover rounded-full"
+              />
+          </a>
+      </div>
+  )
+}
+
+export default Footer
