e9a7adba81e6da9b3a1706076627b98dada0b660
akhoz <adrian25092005@gmail.com>
2023-11-24 23:51:40 -0600

    implementing new features

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Button from "./components/Button.jsx";
 import { moneyFormat} from "./helpers";
 
 function App() {
-    let [amount, setAmount] = useState(5000);
+    const [amount, setAmount] = useState(5000);
+    const [term, setTerm] = useState(6);
+    const [total, setTotal] = useState(0);
 
     function handleChange(event) {
         setAmount(+event.target.value); // + converts string to number
@@ -59,6 +61,34 @@ function App() {
                value={amount}
         />
         <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{moneyFormat(amount)}</p>
+
+        <h2 className="text-center text-xl font-bold text-gray-500">
+            Select a repayment term
+        </h2>
+
+        <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg
+        text-center text-xl font-bold text-indigo-600"
+        value={term}
+        onChange={event => setTerm(+event.target.value)}>
+
+            <option value="6">6 months</option>
+            <option value="12">12 months</option>
+            <option value="24">24 months</option>
+
+        </select>
+
+        <div className="my-5 space-y-3 bg-gray-50 p-5">
+
+            <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+                <span className="text-indigo-600">Payment</span> summary
+            </h2>
+
+            <p className="text-xl font-bold text-gray-500 text-center font-bold">{term} Months</p>
+            <p className="text-xl font-bold text-gray-500 text-center font-bold">Total amount</p>
+            <p className="text-xl font-bold text-gray-500 text-center font-bold">Monthly</p>
+
+        </div>
+
     </div>
     )
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -7,8 +7,28 @@ const moneyFormat = (value) => {
     return formatter.format(value)
 }
 
+const calculateTotalToPay = (amount, term) => {
+    let totalToPay = 0
 
+    if (amount <= 1000) {
+        totalToPay = amount * 1.25
+    } else if (amount > 1000 && amount <= 5000) {
+        totalToPay = amount * 1.20
+    } else {
+        totalToPay = amount * 1.15
+    }
 
+    if (term === 6) {
+        totalToPay *= 1.05
+    } else if (term === 12) {
+        totalToPay *= 1.10
+    } else {
+        totalToPay *= 1.30
+    }
+
+    return totalToPay
+}
 export {
-    moneyFormat
+    moneyFormat,
+    calculateTotalToPay
 }
