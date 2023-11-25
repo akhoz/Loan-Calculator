807adf0e5650a8b23e4ca375ac16366c4d50e2a7
akhoz <adrian25092005@gmail.com>
2023-11-24 21:58:53 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header.jsx";
+
+function App() {
+
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <Header />
+    </div>
+  )
+}
+
+export default App
